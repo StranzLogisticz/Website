be030418a9930f4f82013fe62236cb7684bc18eb
Stranz <admin@stranz.in>
2025-12-20 04:52:56 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,10 +1,9 @@
-// Load header.html into the #header div
 fetch('header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
 
-    // Highlight active link based on current page
+    // Highlight active link
     const currentPage = window.location.pathname.split("/").pop();
     document.querySelectorAll('.nav-links a').forEach(link => {
       if (link.getAttribute('href') === currentPage) {
