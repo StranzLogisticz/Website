b85ee08f51c13426071562a8b993460ecc0787d8
Stranz <admin@stranz.in>
2025-12-20 04:00:34 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,14 +1,6 @@
-<footer>
-  <p>&copy; 2025 Stranz Logistics | info@stranz.in</p>
-  <div class="social-links">
-    <a href="https://www.linkedin.com/company/stranz-logistics" target="_blank">
-      <img src="assets/img/icons/linkedin.png" alt="LinkedIn" />
-    </a>
-    <a href="https://wa.me/919876543210" target="_blank">
-      <img src="assets/img/icons/whatsapp.png" alt="WhatsApp" />
-    </a>
-    <a href="mailto:info@stranz.in">
-      <img src="assets/img/icons/email.png" alt="Email" />
-    </a>
-  </div>
-</footer>
+// Load footer.html into the #footer div
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
